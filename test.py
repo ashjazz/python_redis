f0e3d73f0fb19f4eb77a6be989f750f2050b8253
蒋彦初 <yanchu.jiang@mfashion.com.cn>
2018-06-15 18:25:35 +0800

    eval test

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 import redis
+x = 1
+y = 1
+num1 = eval("x + y")
+print num1
+
+def g():
+    x = 2
+    y = 2
+    num3 = eval("x + y");
+    print num3
+    num2 = eval("x + y", globals())
+    # num2 = eval("x + y", globals(), locals())
+    print num2
+
+g()
